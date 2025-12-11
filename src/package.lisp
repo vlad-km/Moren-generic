3baccdea82b4a5c993476a9db59effdb8631ef19
vlad-km <gm5461@gmail.com>
2025-12-11 15:04:49 +0300

    Create package.lisp

---
+++ src/package.lisp
@@ -0,0 +1,21 @@
+;;; -*- mode:lisp;  coding:utf-8 -*-
+#|
+
+            /\___/\
+            )     (
+           =\     /=                  if this code is not work, i dont know who wrote this code
+             )   (                    Copyright © 2017,2018,2022  @vlad-km
+            /     \                   2017, Original https://github.com/vlad-km/moren-electron
+            )     (                   2022, Code redesign
+           /       \                  Electron >= electron@21.2.2
+           \       /                  JSCL >= version 0.8.2  
+      jgs   \__ __/
+               ))
+              //
+             ((
+              \)
+|#
+
+
+
+;;; EOF
